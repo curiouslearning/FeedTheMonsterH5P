e622745ae5bbc7e531ccbf6cb8d59ff36c363519
Pursotam Singh <35638587+pks99ing@users.noreply.github.com>
2022-02-18 13:44:54 +0530

    commit

--- src/components/Slide.tsx
+++ src/components/Slide.tsx
@@ -1,202 +1,313 @@
-import React, { useEffect, useRef, useState } from 'react';
-import styled from 'styled-components';
-import './app.css';
-import { DndProvider } from 'react-dnd';
-import { HTML5Backend } from 'react-dnd-html5-backend';
-import DragDrop from './dragdrop-stones/DragDrop';
-import Progress from './progress-bar/progress';
-import PuzzelBar, { PuzzelBarHook } from './puzzel-bar/PuzzelBar';
-import ScoreBoard from './score-board/ScoreBoard';
-import PauseMenu from './pause-menu/PauseMenu';
-import PromptText from './prompt-text/PromptText';
-import PopupMenu from './popup-menu/PopupMenu';
-import bg from '../../assets/images/bg.jpg';
-import { url } from 'inspector';
+import React, { useEffect, useRef, useState } from "react";
+import styled from "styled-components";
+import "./app.css";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import DragDrop from "./dragdrop-stones/DragDrop";
+import Progress from "./progress-bar/progress";
+import PuzzelBar, { PuzzelBarHook } from "./puzzel-bar/PuzzelBar";
+import ScoreBoard from "./score-board/ScoreBoard";
+import PauseMenu from "./pause-menu/PauseMenu";
+import PromptText from "./prompt-text/PromptText";
+import PopupMenu from "./popup-menu/PopupMenu";
+import bg from "../../assets/images/bg.jpg";
+import { url } from "inspector";
+import AnimationType from "./animations/AnimationType";
 
 let audio: HTMLAudioElement = null;
 let initialTime = 10;
 let id: NodeJS.Timeout;
 
 const Wrapper = styled.div`
-    height: 600px;
-    width: 100%;
-    position: relative;
+  height: 600px;
+  width: 100%;
+  position: relative;
 `;
 
 const DragDropComp = (props: any) => {
+  const [timeOver, setTimeOver] = useState(false);
+  const [correctDrop, setCorrectDrop] = useState(false);
+  const [levelCount, setLevelCount] = useState(0);
+  const [currentProgressCount, setProgressCount] = useState(initialTime);
+  const [prompted, setPromted] = useState(props.promptVisibility);
+  const [activeIndicators, setActiveIndicator] = useState(0);
+  const [isMenuPopup, setPauseMenu] = useState(false);
 
-    const [timeOver, setTimeOver] = useState(false);
-    const [correctDrop, setCorrectDrop] = useState(false);
-    const [levelCount, setLevelCount] = useState(0);
-    const [currentProgressCount, setProgressCount] = useState(initialTime);
-    const [prompted, setPromted] = useState(props.promptVisibility);
-    const [activeIndicators, setActiveIndicator] = useState(0);
-    const [ isMenuPopup, setPauseMenu ] = useState(false);
+  const onClickRestart = () => {
+    setTimeout(() => {
+      setLevelCount(0);
+      setPauseMenu(false);
+      setProgressCount(initialTime);
+      setActiveIndicator(0);
+    }, 1000);
+  };
 
-
-    const onClickRestart = () => {
-        setTimeout(() => {
-            setLevelCount(0);
-            setPauseMenu(false);
-            setProgressCount(initialTime);
-            setActiveIndicator(0)
-        }, 1000)
+  const onClickPauseMenu = () => {
+    if (!isMenuPopup) {
+      setPauseMenu(true);
+      if (props.playing) {
+        audio.pause();
+      }
     }
 
-    const onClickPauseMenu = () => {
-        
-        if (!isMenuPopup) {
-            setPauseMenu(true);
-            if (props.playing) {
-                audio.pause();
-            }
-        }
+    if (isMenuPopup) {
+      setPauseMenu(false);
+    }
+  };
 
-        if (isMenuPopup) {
-            setPauseMenu(false);
-        }
+  const answerDrop = () => {
+    setCorrectDrop(true);
+  };
+
+  const levelUp = () => {
+    setTimeout(() => {
+      setLevelCount((preCount) => preCount + 1);
+      setCorrectDrop(false);
+      setProgressCount(initialTime);
+      setActiveIndicator((pre) => pre + 1);
+      setPromted(true);
+      props.stopPlaying();
+      props.playAudio();
+    }, 4000);
+  };
+
+  const timer = () => {
+    if (props.playing && !isMenuPopup) {
+      setProgressCount((preValue) => preValue - 0.5);
+    }
+  };
+
+  useEffect(() => {
+    if (props.playing) {
+      console.log("times");
+      props.monsterRef.current.style.display = "none";
+      if (prompted) {
+        setPromted(false);
+      }
+    } else {
+      props.monsterRef.current.style.display = "block";
     }
 
-    const answerDrop = () => {
-        setCorrectDrop(true);
+    if (!props.start) {
+      setProgressCount(10);
+      props.stopPlaying();
+      return;
     }
 
-    const levelUp = () => {
-        setTimeout(() => {
-            setLevelCount(preCount => preCount + 1)
-            setCorrectDrop(false)
-            setProgressCount(initialTime);
-            setActiveIndicator(pre => pre + 1);
-            setPromted(true);
-            props.stopPlaying();
-            props.playAudio();
-        }, 4000)
+    if (currentProgressCount <= 0 && !timeOver) {
+      setTimeout(() => {
+        // setProgressCount(10);
+        // props.stopPlaying()
+        levelUp();
+        return;
+      }, 1000);
     }
 
-    const timer = () => {
-        if (props.playing && !isMenuPopup) {
-            setProgressCount(preValue => preValue - 0.5);
-        }        
+    if (currentProgressCount <= 0 || correctDrop) {
+      setTimeOver(false);
+      return;
     }
+    id = setInterval(timer, 500, props.start);
 
-    useEffect(() => {
+    return () => clearInterval(id);
+  }, [
+    currentProgressCount,
+    props.start,
+    props.playing,
+    timeOver,
+    correctDrop,
+    isMenuPopup,
+  ]);
 
-        if (props.playing) {
-            if (prompted) {
-                setPromted(false);
-            }
-        }
-
-        if (!props.start) {
-            setProgressCount(10);
-            props.stopPlaying()
-            return;
-        }
-
-        if (currentProgressCount <= 0 && !timeOver) {
-            setTimeout(() => {
-                // setProgressCount(10);
-                // props.stopPlaying()
-                levelUp()
-                return
-            }, 1000)
-        }
-
-        if (currentProgressCount <= 0 || correctDrop) {
-            setTimeOver(false)
-            return;
-        }
-        id = setInterval(timer,500, props.start);
-
-        return () => clearInterval(id);
-    }, [currentProgressCount, props.start, props.playing, timeOver, correctDrop, isMenuPopup])
-
-    return <div>
-        <div style={{display: 'flex', justifyContent: "space-between", marginInline: "50px", marginTop: "20px"}}>
-            <PuzzelBar puzzelCount={4} activeIndicators={activeIndicators}/>
-            <ScoreBoard score={280}/>
-            <PauseMenu onClickPauseMenu={onClickPauseMenu} />
-        </div>
-        {isMenuPopup ? <PopupMenu onClickPauseMenu={onClickPauseMenu} onClickRestart={onClickRestart} nextLevel={props.nextLevel}/> : <></>} 
-        <Progress done={(currentProgressCount * 10).toString()} />
-        <PromptText letter={ props.puzzles[levelCount].prompt.PromptText} /> 
-        {prompted ? <></> : <DndProvider backend={HTML5Backend}>
-            <div className="dragAndDrop" style={{height: "200px"}}>
-                <DragDrop timeOver={timeOver} answerDrop={answerDrop} startDrag={false} props={props.puzzles[levelCount]} changePuzzel={levelUp} levelCount={levelCount} isMenuOpen={isMenuPopup} />
-            </div>
-        </DndProvider>}
+  return (
+    <div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          marginInline: "50px",
+          marginTop: "20px",
+        }}
+      >
+        <PuzzelBar puzzelCount={4} activeIndicators={activeIndicators} />
+        <ScoreBoard score={280} />
+        <PauseMenu onClickPauseMenu={onClickPauseMenu} />
+      </div>
+      {isMenuPopup ? (
+        <PopupMenu
+          onClickPauseMenu={onClickPauseMenu}
+          onClickRestart={onClickRestart}
+          nextLevel={props.nextLevel}
+        />
+      ) : (
+        <></>
+      )}
+      <Progress done={(currentProgressCount * 10).toString()} />
+      <PromptText letter={props.puzzles[levelCount].prompt.PromptText} />
+      {prompted ? (
+        <></>
+      ) : (
+        <DndProvider backend={HTML5Backend}>
+          <div className="dragAndDrop" style={{ height: "200px" }}>
+            <DragDrop
+              timeOver={timeOver}
+              answerDrop={answerDrop}
+              startDrag={false}
+              props={props.puzzles[levelCount]}
+              changePuzzel={levelUp}
+              levelCount={levelCount}
+              isMenuOpen={isMenuPopup}
+            />
+          </div>
+        </DndProvider>
+      )}
     </div>
-}
-
+  );
+};
 
 const SlideComponent = (props: any) => {
-    const { data } = props;
-    var audFile: string;
+  const { data } = props;
+  var audFile: string;
 
-    const [playing, setPlaying] = useState(false);
-    const [start, setStart] = useState(false);
+  const [playing, setPlaying] = useState(false);
+  const [start, setStart] = useState(false);
 
-    const stopPlaying = () => {
-        if (playing) {
-            setPlaying(false);
-        }
+  const stopPlaying = () => {
+    if (playing) {
+      setPlaying(false);
     }
+  };
 
-    useEffect(() => {
-        setStart(false)
-        return () => {
-            if (playing || audio!=null) {
-                audio.pause()
-            }
-            audio = null
-            initialTime = 10
-            clearInterval(id);
-            setPlaying(false)
-        } 
+  useEffect(() => {
+    setStart(false);
+    return () => {
+      if (playing || audio != null) {
+        audio.pause();
+      }
+      audio = null;
+      initialTime = 10;
+      clearInterval(id);
+      setPlaying(false);
+    };
+  }, [props.started]);
 
-    },[props.started])
-    
-    const playAudio = () => {
-        audio = new Audio("https://www.kozco.com/tech/piano2.wav")
-        var playPromise =  audio.play();
-        if (playPromise !== undefined) {
-            playPromise.then(() => {
-                // setPlaying(true);
-            }).catch((err: any) => {
-                console.log(err);
-            });
-        }
-        audio.addEventListener('ended', ()=>{
-            setPlaying(true);
+  const playAudio = () => {
+    audio = new Audio("https://www.kozco.com/tech/piano2.wav");
+    var playPromise = audio.play();
+    if (playPromise !== undefined) {
+      playPromise
+        .then(() => {
+          // setPlaying(true);
         })
+        .catch((err: any) => {
+          console.log(err);
+        });
     }
+    audio.addEventListener("ended", () => {
+      setPlaying(true);
+    });
+  };
 
-    const onStartClick = () => {
-        setTimeout(() => {
-            setStart(true);
-        }, 0)
-        playAudio();
-    }
+  const onStartClick = () => {
+    setTimeout(() => {
+      setStart(true);
+    }, 0);
+    playAudio();
+  };
 
-    return (
-        <Wrapper>
-            <img src={bg} style={{position: "absolute", width: "100%", height: "600px", zIndex: -2}}></img>
-            {data.audio && data.audio.length > 0 ? "" : <audio src={audFile} autoPlay></audio>}
-                { start ? <></> :
-                    <div style={{height: "100%", backgroundImage: `url(${props.images})`, backgroundPosition: 'center', backgroundRepeat: 'no-repeat', backgroundSize: 'cover'}}>
-                        <div style={{width: "100%", height: "100%", background: "rgba(0,0,0,0.3)", display: "flex", flexDirection: "column", justifyContent: "center", transform: "translateX(0px)"}}>
-                            <h1 style={{textAlign: "center", fontSize: "2.857em", color: "white"}}>{"Level - " + data.LevelNumber}</h1>
-                            <button onClick={() => onStartClick()} style={{marginInline: "auto"}}>Start</button>
-                        </div>
-                    </div>
-                }
-            {!start ? <div></div> : 
-                <DragDropComp playing={playing} start={start} levelType={data.LevelMeta.LevelType == "LetterInWord" ? true : false} promptVisibility={data.LevelMeta.PromptType == "Visible" ? true : false} puzzles={data.Puzzles} stopPlaying={stopPlaying} playAudio={playAudio} nextLevel={props.nextLevel} />
+  const monsterRef = useRef();
+  return (
+    <Wrapper>
+      <img
+        src={bg}
+        style={{
+          position: "absolute",
+          width: "100%",
+          height: "600px",
+          zIndex: -2,
+        }}
+      ></img>
+      {data.audio && data.audio.length > 0 ? (
+        ""
+      ) : (
+        <audio src={audFile} autoPlay></audio>
+      )}
+      {start ? (
+        <></>
+      ) : (
+        <div
+          style={{
+            height: "100%",
+            backgroundImage: `url(${props.images})`,
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
+          }}
+        >
+          <div
+            style={{
+              width: "100%",
+              height: "100%",
+              background: "rgba(0,0,0,0.3)",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              transform: "translateX(0px)",
+            }}
+          >
+            <h1
+              style={{
+                textAlign: "center",
+                fontSize: "2.857em",
+                color: "white",
+              }}
+            >
+              {"Level - " + data.LevelNumber}
+            </h1>
+            <button
+              onClick={() => onStartClick()}
+              style={{ marginInline: "auto" }}
+            >
+              Start
+            </button>
+          </div>
+        </div>
+      )}
+      {!start ? (
+        <div></div>
+      ) : (
+        <>
+          <DragDropComp
+            playing={playing}
+            start={start}
+            levelType={
+              data.LevelMeta.LevelType == "LetterInWord" ? true : false
             }
-        </Wrapper>
-    );
-}
+            promptVisibility={
+              data.LevelMeta.PromptType == "Visible" ? true : false
+            }
+            puzzles={data.Puzzles}
+            stopPlaying={stopPlaying}
+            playAudio={playAudio}
+            nextLevel={props.nextLevel}
+            monsterRef={monsterRef}
+          />
+          <div
+            ref={monsterRef}
+            style={{
+              width: "300px",
+              height: "100px",
+              top: "50%",
+              left: "30%",
+              position: "absolute",
+            }}
+          >
+            <AnimationType type="idle" />
+          </div>
+        </>
+      )}
+    </Wrapper>
+  );
+};
 
 export default SlideComponent;
-
-
--- src/components/dragdrop-stones/DragDrop.tsx
+++ src/components/dragdrop-stones/DragDrop.tsx
@@ -1,125 +1,147 @@
 import React, { useEffect, useRef, useState } from "react";
-import './dragdrop-balls.css';
+import "./dragdrop-balls.css";
 import classNames from "classnames";
 import AnimationType from "../animations/AnimationType";
 
-let optionDataSet: { id: number; alphabet: string; }[] = []
+let optionDataSet: { id: number; alphabet: string }[] = [];
 
-const DragDrop = ({timeOver, answerDrop, startDrag, props, changePuzzel, levelCount, isMenuOpen} : {timeOver : boolean, answerDrop : Function, startDrag: boolean, props: any, changePuzzel: Function, levelCount: number, isMenuOpen: boolean}) => {
+const DragDrop = ({
+  timeOver,
+  answerDrop,
+  startDrag,
+  props,
+  changePuzzel,
+  levelCount,
+  isMenuOpen,
+}: {
+  timeOver: boolean;
+  answerDrop: Function;
+  startDrag: boolean;
+  props: any;
+  changePuzzel: Function;
+  levelCount: number;
+  isMenuOpen: boolean;
+}) => {
+  let options = [props.targetstones[0], ...props.foilstones];
 
-    let options = [props.targetstones[0], ...props.foilstones]
+  const [dataList, setDataList] = useState(options);
+  const [dragging, setDragging] = useState(false);
+  const [dropped, setDropped] = useState(false);
+  const [animationType, setAnimationType] = useState("idle");
 
-    const [dataList, setDataList] = useState(options);
-    const [dragging, setDragging] = useState(false);
-    const [dropped, setDropped] = useState(false);
-    const [animationType, setAnimationType] = useState('idle');
+  const dragItem = useRef();
+  const dragId = useRef();
 
-    const dragItem = useRef();
-    const dragId = useRef();
+  const optionCollectData = (options: string | any[]) => {
+    for (let i = 0; i < options.length; i++) {
+      let incomingData = {
+        id: 100,
+        alphabet: "",
+      };
 
-    const optionCollectData = (options: string | any[]) => {
-        for (let i = 0; i< options.length; i++) {
-            let incomingData = {
-                id: 100,
-                alphabet: ""
-            }
-
-            incomingData.id = i;
-            incomingData.alphabet = options[i].StoneText;
-            optionDataSet.push(incomingData);
-
-        }
-        setDataList(optionDataSet)
+      incomingData.id = i;
+      incomingData.alphabet = options[i].StoneText;
+      optionDataSet.push(incomingData);
     }
+    setDataList(optionDataSet);
+  };
 
-    useEffect(() => {
-        optionCollectData(options)
-        return () => {
-            options=[]
-            optionDataSet=[]
-        }
-    }, [levelCount])
+  useEffect(() => {
+    optionCollectData(options);
+    return () => {
+      options = [];
+      optionDataSet = [];
+    };
+  }, [levelCount]);
 
-    const handleDragStart = (data: any, e: any, id: any ) => {
-        dragItem.current = data;
-        dragId.current = id;
-        setTimeout(() => {
-            setDragging(true);
-        }, 0)
+  const handleDragStart = (data: any, e: any, id: any) => {
+    dragItem.current = data;
+    dragId.current = id;
+    setTimeout(() => {
+      setDragging(true);
+    }, 0);
+  };
+
+  const handleDragEnd = () => {
+    setDragging(false);
+    dragItem.current = null;
+  };
+
+  const getStyles = (params: any, index: any) => {
+    console.log(index);
+    console.log(params);
+    const currentItem = dragItem.current;
+    if (currentItem == params) {
+      let str = "no-ball";
+      return str.concat(index.toString());
     }
+    let str = "ball";
+    return str.concat(index.toString());
+  };
 
-    const handleDragEnd = () => {
-        setDragging(false)
-        dragItem.current = null;
+  const checkResult = (dropData: any) => {
+    // console.log("hihihih")
+    console.log(props.targetstones[0].StoneText, "resultin progress", dropData);
+    let targetStone = props.targetstones[0].StoneText;
+    optionDataSet.filter((item) => item.id != dragId.current);
+    optionDataSet = [];
+    setDropped(true);
+    answerDrop();
+    changePuzzel();
+    setDropped(false);
+    setDragging(false);
+
+    if (targetStone === dropData) {
+      setAnimationType("eat");
+    } else {
+      setAnimationType("spit");
     }
-
-    const getStyles = (params: any, index: any) => {
-        console.log(index)
-        console.log(params)
-        const currentItem = dragItem.current;
-        if (currentItem == params) {
-            let str = "no-ball"
-            return str.concat(index.toString());
-        }
-        let str = "ball";
-        return str.concat(index.toString());
-    }
-
-    const checkResult = (dropData: any) => {
-        // console.log("hihihih")
-        console.log(props.targetstones[0].StoneText, "resultin progress", dropData);
-        let targetStone = props.targetstones[0].StoneText;
-        optionDataSet.filter((item) => item.id != dragId.current)
-        optionDataSet = [];
-        setDropped(true);
-        answerDrop();
-        changePuzzel();
-        setDropped(false);
-        setDragging(false);
-
-        if (targetStone === dropData) {
-            setAnimationType('eat');
-        } else {
-            setAnimationType('spit');
-        }
-        dragItem.current = null;
-    }
-
-    return (
-        <>
-            <div  style={{
-                    width: '300px',
-                    height: '100px',
-                    top:'50%',
-                    left:'30%',
-                    position:'absolute'   
-                }} onDragOver={(e) => {
-                    console.log("onDragOver::")
-                    e.stopPropagation();
-                    e.preventDefault();
-                }} onDrop={(e) => {
-                    checkResult(e.dataTransfer.getData("item.alphabet"));
-                    console.log("::onDrop")
-                }}>
-                
-                <AnimationType type={animationType}/>
-            </div>
-            {optionDataSet.map((item, index) => {
-                return <div 
-                    className={classNames(dragging ? getStyles(item.alphabet, index) :  "ball"+index)} 
-                    draggable = {!timeOver && !isMenuOpen} 
-                    key={item.id} 
-                    onDragEnd={(e) => {
-                        handleDragEnd();
-                    }} onDragStart = {(e) => {
-                        handleDragStart(item.alphabet, e, item.id)
-                        e.dataTransfer.setData("item.alphabet", item.alphabet)
-                    }}>
-                    <p className="stones-letter">{item.alphabet}</p>
-                </div>
-            })}
-        </>
-    );
-}
+    dragItem.current = null;
+  };
+  return (
+    <>
+      <div
+        style={{
+          width: "300px",
+          height: "100px",
+          top: "50%",
+          left: "30%",
+          position: "absolute",
+        }}
+        onDragOver={(e) => {
+          console.log("onDragOver::");
+          e.stopPropagation();
+          e.preventDefault();
+        }}
+        onDrop={(e) => {
+          checkResult(e.dataTransfer.getData("item.alphabet"));
+          console.log("::onDrop");
+        }}
+      >
+        <AnimationType type={animationType} />
+      </div>
+      {optionDataSet.map((item, index) => {
+        return (
+          <div
+            className={classNames(
+              dragging ? getStyles(item.alphabet, index) : "ball" + index
+            )}
+            draggable={!timeOver && !isMenuOpen}
+            key={item.id}
+            onDragEnd={(e) => {
+              handleDragEnd();
+            }}
+            onDragStart={(e) => {
+              handleDragStart(item.alphabet, e, item.id);
+              e.dataTransfer.setData("item.alphabet", item.alphabet);
+            }}
+          >
+            <p className="stones-letter">{item.alphabet}</p>
+          </div>
+        );
+      })}
+    </>
+  );
+};
 
 export default DragDrop;
