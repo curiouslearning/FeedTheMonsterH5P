c5bf83cc2d91f2929099f3bb1f1d54a98ce0d44e
Mary-Harshitha <62737530+Mary-Harshitha@users.noreply.github.com>
2022-03-10 14:38:23 +0530

    feedback phrase commit

--- src/components/prompt-text/PromptText.tsx
+++ src/components/prompt-text/PromptText.tsx
@@ -15,7 +15,7 @@ const PromptText = (props: any) => {
         nonHighlightText = letter.slice(1, letter.length);
     }
     
-    return <div onClick={playAudio} className='ans-pop-up' style={{margin: "auto", display: "flex", width: "100px", height: "60px", justifyContent: "center", marginTop: "20px", background: "grey"}}>
+    return <div onClick={playAudio} className='ans-pop-up' style={{margin: "auto", display: "flex", width: "100px", height: "50px", justifyContent: "center", marginTop: "20px", background: "grey"}}>
         {textVisbility ? <p className='ans-pop-up-text' style={{fontSize: "1.857em", textAlign: "center"}}>{levelType == "LetterInWord" ? <><span style={{color: "red"}}>{highlightText}</span><span>{nonHighlightText}</span></> : props.letter}</p> : <p style={{fontSize: "1.857em", textAlign: "center"}}>▶️</p>}
     </div>;
 }
