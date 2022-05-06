ca23501fc7ebde507d390bb9b816c1a5bd7aafa6
Vinay <62736791+vinay961861@users.noreply.github.com>
2022-05-06 13:40:19 +0530

    some changes

--- src/components/progress-bar/progress.tsx
+++ src/components/progress-bar/progress.tsx
@@ -1,42 +1,48 @@
-import React from 'react';
-import './progress.css';
-import emptyTimer from '../../../assets/images/timer_empty.png';
-import timer from '../../../assets/images/timer.png';
-import { getImagePath } from '../../app';
+import React from "react";
+import "./progress.css";
+import emptyTimer from "../../../assets/images/timer_empty.png";
+import timer from "../../../assets/images/timer.png";
+import { getImagePath } from "../../app";
 
-const Progress = ({done} : {done : string}) => {
-
+const Progress = ({ done }: { done: string }) => {
   const { style } = ProgressHooks(done);
 
   return (
-    <div className='time-container'>
-        {/* <div className='timer' style={{backgroundImage: `url(${timer})`, backgroundRepeat: "no-repeat", backgroundSize: "contain"}}></div> */}
-        <div className="progress" style={{backgroundImage: `url(${getImagePath()+'timer_empty.png'})`, transform: "scale(3.4,2.3)", backgroundRepeat: "no-repeat", backgroundSize: "contain"}}>
-          <div className="progress-done" style={style}></div>
-        </div>
+    <div className="time-container">
+      {/* <div className='timer' style={{backgroundImage: `url(${timer})`, backgroundRepeat: "no-repeat", backgroundSize: "contain"}}></div> */}
+      <div
+        className="progress"
+        style={{
+          backgroundImage: `url(${getImagePath() + "timer_empty.png"})`,
+          width:'100%',
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "100% 100%",
+        }}
+      >
+        <div className="progress-done" style={style}></div>
+      </div>
     </div>
   );
-}
+};
 
 const ProgressHooks = (done: string) => {
-  const [style, setStyle] = React.useState({});  
+  const [style, setStyle] = React.useState({});
 
   const decrementBar = () => {
-      setTimeout(() => {
+    setTimeout(() => {
       const newStyle = {
-          opacity: 1,
-          height: 15,
-          width: `${done}%`
-      }
-      
+        opacity: 1,
+        height: 25,
+        width: `${done}%`,
+      };
+
       setStyle(newStyle);
-      }, 200);
-  }
+    }, 200);
+  };
 
   decrementBar();
 
-  return { style }; 
-}
+  return { style };
+};
 
 export default Progress;
-
