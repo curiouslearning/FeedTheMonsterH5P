83fc9f8287f5eef5b3dfd5b871429b181fff02c8
Vinay <62736791+vinay961861@users.noreply.github.com>
2022-07-07 16:22:09 +0530

    Update AnimationType.tsx

--- src/components/animations/AnimationType.tsx
+++ src/components/animations/AnimationType.tsx
@@ -8,96 +8,43 @@ import sad from "../../../assets/images/sad.png";
 import idle from "../../../assets/images/idle.png";
 import { SpriteAnimationComponent } from "./SpriteAnimation";
 import { getImagePath } from "../../app";
-import { base64Images } from "../profile/SelectProfile";
 
 var spritesheetobj: Spritesheet;
 const AnimationType = (props: any) => {
   const animationType = props.type;
-  const internalPhaseNo = props.getPhaseCharNo + 1;
-  console.log(props.getPhaseCharNo);
-  console.log(internalPhaseNo);
+  const internalPhaseNo=props.getPhaseCharNo+1;
+  console.log(props.getPhaseCharNo)
+  console.log(internalPhaseNo)
   console.log(props.type);
-  console.log(getImagePath() + "idle1" + internalPhaseNo + ".png");
+  console.log(getImagePath()+'idle1'+internalPhaseNo+'.png')
   switch (props.type) {
     case "eat": {
-      return (
-        <SpriteAnimationComponent
-          spImage={
-            localStorage.getItem('eat14')
-          }
-          nFrames={18}
-        />
-      );
+      return <SpriteAnimationComponent spImage={getImagePath()+'eat1'+internalPhaseNo+'.png'} nFrames={18} />;
     }
     case "spit": {
-      return (
-        <SpriteAnimationComponent
-          spImage={
-            localStorage.getItem('spit14')
-          }
-          nFrames={18}
-        />
-      );
+      return <SpriteAnimationComponent spImage={getImagePath()+'spit1'+internalPhaseNo+'.png'}  nFrames={18} />;
     }
-
+    
     case "idle": {
-      return (
-        <SpriteAnimationComponent
-          spImage={
-            localStorage.getItem('idle14')
-          }
-          nFrames={18}
-        />
-      );
+      return <SpriteAnimationComponent spImage={getImagePath()+'idle1'+internalPhaseNo+'.png'} nFrames={18} />;
     }
     case "happy": {
       return (
-        <SpriteAnimationComponent
-          spImage={
-            localStorage.getItem('happy')
-          }
-          nFrames={11}
-          height={662}
-        />
+        <SpriteAnimationComponent spImage={getImagePath()+'happy1'+internalPhaseNo+'.png'} nFrames={11} height={662}/>
       );
     }
     case "sad": {
       return (
-        <SpriteAnimationComponent
-          spImage={
-            base64Images.get("sad1" + internalPhaseNo + ".png")
-              ? base64Images.get("sad1" + internalPhaseNo + ".png")
-              : getImagePath() + "sad1" + internalPhaseNo + ".png"
-          }
-          nFrames={11}
-          height={662}
-        />
+        <SpriteAnimationComponent spImage={getImagePath()+'sad1'+internalPhaseNo+'.png'} nFrames={11} height={662} />
       );
     }
     case "profile": {
       return (
-        <SpriteAnimationComponent
-          spImage={
-            base64Images.get("idle14.png")
-              ? base64Images.get("idle14.png")
-              : getImagePath() + "idle14.png"
-          }
-          nFrames={11}
-          height={1080}
-        />
+        <SpriteAnimationComponent spImage={getImagePath()+'idle14.png'} nFrames={11} height={1080} />
       );
     }
     default: {
-      return (
-        <SpriteAnimationComponent
-        spImage={
-          base64Images.get("egg.png")
-            ? base64Images.get("egg.png")
-            : getImagePath() + "egg.png"
-        }
-          nFrames={18}
-        />
-      );
+      return <SpriteAnimationComponent spImage={getImagePath()+'egg.png'} nFrames={18} />;
     }
   }
 };
