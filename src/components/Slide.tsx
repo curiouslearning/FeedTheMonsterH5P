8dadfb100f24c9585987539005d3ae62daea1e36
amitsinghsutara <102941445+amitsinghsutara@users.noreply.github.com>
2022-07-08 12:08:23 +0530

    AdditionalFixes

--- src/components/Slide.tsx
+++ src/components/Slide.tsx
@@ -48,6 +48,7 @@ const Wrapper = styled.div`
   height: 100vh;
   width: 100%;
   position: relative;                      
+  overflow: hidden;       
 `;
 const DragDropComp = (props: any) => {
   console.log("DRAGDROPCOMP PROPS ==> ", props);
@@ -284,6 +285,7 @@ const DragDropComp = (props: any) => {
     }
   };
   return isLevelEnded ? (
+    document.getElementById("exitButton").style.color="white",
     <EndLevelComponent
       score={score}
       lengthOfCurrentLevel={props.lengthOfCurrentLevel}
