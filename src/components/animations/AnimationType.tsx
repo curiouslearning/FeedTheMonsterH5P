eb27bd6d767fcf118c4481cc1ff51ec55c327034
Rakshith <62013445+rakshith-arch@users.noreply.github.com>
2022-07-14 15:08:04 +0530

    changes

--- src/components/animations/AnimationType.tsx
+++ src/components/animations/AnimationType.tsx
@@ -6,8 +6,10 @@ import egg from "../../../assets/images/eggAnimation.png";
 import happy from "../../../assets/images/happy.png";
 import sad from "../../../assets/images/sad.png";
 import idle from "../../../assets/images/idle.png";
-import { SpriteAnimationComponent } from "./SpriteAnimation";
+// import { SpriteAnimationComponent } from "./SpriteAnimation";
 import { getImagePath } from "../../app";
+import { SpriteAnimationComponent } from "./SpriteAnimation";
+import SampleAnimation from "./SampleAnimation";
 
 var spritesheetobj: Spritesheet;
 const AnimationType = (props: any) => {
@@ -19,29 +21,35 @@ const AnimationType = (props: any) => {
   console.log(getImagePath()+'idle1'+internalPhaseNo+'.png')
   switch (props.type) {
     case "eat": {
-      return <SpriteAnimationComponent spImage={getImagePath()+'eat1'+internalPhaseNo+'.png'} nFrames={18} />;
+      // return <SpriteAnimationComponent spImage={getImagePath()+'eat1'+internalPhaseNo+'.png'} nFrames={18} />;
+      return <SampleAnimation imgs={getImagePath()+'eat1'+internalPhaseNo+'.png'}/>
     }
     case "spit": {
-      return <SpriteAnimationComponent spImage={getImagePath()+'spit1'+internalPhaseNo+'.png'}  nFrames={18} />;
+      // return <SpriteAnimationComponent spImage={getImagePath()+'spit1'+internalPhaseNo+'.png'}  nFrames={18} />;
+      return <SampleAnimation imgs={getImagePath()+'spit1'+internalPhaseNo+'.png'}/>
     }
     
     case "idle": {
-      return <SpriteAnimationComponent spImage={getImagePath()+'idle1'+internalPhaseNo+'.png'} nFrames={18} />;
+      // return <SpriteAnimationComponent spImage={getImagePath()+'idle1'+internalPhaseNo+'.png'} nFrames={18} />;
+      return <SampleAnimation imgs={getImagePath()+'idle1'+internalPhaseNo+'.png'}/>
     }
     case "happy": {
-      return (
-        <SpriteAnimationComponent spImage={getImagePath()+'happy1'+internalPhaseNo+'.png'} nFrames={11} height={662}/>
-      );
+      // return (
+      //   <SpriteAnimationComponent spImage={getImagePath()+'happy1'+internalPhaseNo+'.png'} nFrames={11} height={662}/>
+      // );
+      return <SampleAnimation imgs={getImagePath()+'happy1'+internalPhaseNo+'.png'}/>
     }
     case "sad": {
-      return (
-        <SpriteAnimationComponent spImage={getImagePath()+'sad1'+internalPhaseNo+'.png'} nFrames={11} height={662} />
-      );
+      // return (
+      //   <SpriteAnimationComponent spImage={getImagePath()+'sad1'+internalPhaseNo+'.png'} nFrames={11} height={662} />
+      // );
+      return <SampleAnimation imgs={getImagePath()+'sad1'+internalPhaseNo+'.png'}/>
     }
     case "profile": {
-      return (
-        <SpriteAnimationComponent spImage={getImagePath()+'idle14.png'} nFrames={11} height={1080} />
-      );
+      // return (
+      //   <SpriteAnimationComponent spImage={getImagePath()+'idle14.png'} nFrames={11} height={1080} />
+      // );
+      return <SampleAnimation imgs={getImagePath()+'idle14.png'}/>
     }
     default: {
       return <SpriteAnimationComponent spImage={getImagePath()+'egg.png'} nFrames={18} />;
---
+++ src/components/animations/SampleAnimation.tsx
@@ -0,0 +1,28 @@
+import './SampleAnimation.css';
+import img from '../../../assets/images/eat12.png'
+import img1 from '../../../assets/images/eat13.png';
+import img2 from '../../../assets/images/eat14.png';
+import { useState } from 'react';
+import React from 'react';
+import { getImagePath } from "../../app";
+
+const SampleAnimation=(props:any) =>{
+  console.log(props.imgs)
+  console.log(props)
+  const [monster, setMonster] = useState(getImagePath()+'eat12'+'.png');
+  console.log(monster)
+  return (
+    // <div style={{overflow:'auto',height:'200px',width:'200px'}}>
+    <div>
+      {/* <button onClick={() => {
+        setMonster(img);
+        setTimeout(() => {
+          setMonster(img1);
+        }, 2000)
+      }}>change Img</button> */}
+      <div className='monster' style={{background: `transparent url(${monster}) 0 0 no-repeat`}}></div>
+    </div>
+    // </div>
+  );
+}
+export default SampleAnimation;
